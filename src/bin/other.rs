ffab3a446b730ab5578c5928985efef99df913d8
Waqar VSCODE <waqarshujrah@gmail.com>
2024-03-30 23:02:12 +0000

    something here8

--- src/bin/other.rs
+++ src/bin/other.rs
@@ -1,5 +1,17 @@
 fn main(){
 
     println!("i am other file!");
-    
+
+    let float_val: f64 = 5.5f64;
+    println!("flaot val is {}", float_val);
+
+    {
+        const AGE:i32= 37;
+
+        let  float_val = 3.4f32;
+        println!("in the block! {}", float_val);
+
+        println!("age const is  {}", AGE);
+
+    }
 }
---
+++ src/bin/touples.rs
@@ -0,0 +1,8 @@
+
+
+
+fn main(){
+    let personal_data = ("waqar", 34);
+    println!("name is {} and age is {}", personal_data.0, personal_data.1 );
+
+}
